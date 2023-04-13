46013ea6359b2dd4b740ab6cf2177431d04b3f3d
Tatyana Shkolnik <t.shkolnik@tinkoff.ru>
2023-04-13 15:15:21 +0300

    Setting project dependencies and dependencies through modules

---
+++ common/common-ui/src/main/java/com/example/vmeste/common/ui/resources.kt
@@ -0,0 +1,10 @@
+package com.example.vmeste.common.ui
+
+/**
+ * @author t.shkolnik
+ */
+typealias CommonUiColor = R.color
+typealias CommonUiDrawable = R.drawable
+typealias CommonUiId = R.id
+typealias CommonUiLayout = R.layout
+typealias CommonUiString = R.string
